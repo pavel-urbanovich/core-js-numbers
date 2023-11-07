a171fe127e0fa08009324635d11bee8d8b61b4e7
Pavel Urbanovich <yrbik@mail.ru>
2023-11-07 21:08:14 +0300

    feat: add solution of 29 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -380,8 +380,9 @@ function numberToStringInBase(number, base) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  // throw new Error('Not implemented');
+  return Number.parseFloat(number).toExponential(fractionDigits);
 }
 
 /**
@@ -395,8 +396,9 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  // throw new Error('Not implemented');
+  return Number.parseFloat(number).toFixed(fractionDigits);
 }
 
 /**
@@ -411,8 +413,9 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  // throw new Error('Not implemented');
+  return number.toPrecision(precision);
 }
 
 /**
