dff68ba437b8c45c71548f295c32e79637502ec0
Pavel Urbanovich <yrbik@mail.ru>
2023-11-07 13:02:36 +0300

    feat: add solution 9 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -128,8 +128,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return Math.abs(value) % 10;
 }
 
 /**
@@ -143,8 +143,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  // throw new Error('Not implemented');
+  return +value;
 }
 
 /**
