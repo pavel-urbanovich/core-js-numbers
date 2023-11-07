f41f17f2045ddb4af88632f2d6f9a06fa8d1aa96
Pavel Urbanovich <yrbik@mail.ru>
2023-11-07 14:19:41 +0300

    feat: add solution  of 13 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -161,8 +161,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  // throw new Error('Not implemented');
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -182,8 +183,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -203,8 +205,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  if (n === 2 || n === 3 || n === 7) {
+    return true;
+  }
+  if (n % 2 === 0 || n % 3 === 0 || n % 7 === 0) {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -222,8 +231,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // throw new Error('Not implemented');
+  if (Number.isNaN(+value)) {
+    return def;
+  }
+  return Number(value);
 }
 
 /**
